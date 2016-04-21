6e03c41a754edc5178c56259b9d0ea095553e140
Skip Rhudy <skip.rhudy.consultant@bluefly.com>
2016-04-21 10:56:27 -0500

    minor refactor and comments

--- lib/app.rb
+++ lib/app.rb
@@ -7,6 +7,9 @@ products_hash = JSON.parse(file)
 # Print today's date
 puts DateTime.now.strftime("%Y-%m-%d-%H:%M")
 
+#######################################
+#this module is a utility to format dollar
+#data to always have 2 decimal places
 module ToyMoney
   def format(value)
     val = sprintf("%.2f", value)
@@ -14,8 +17,16 @@ module ToyMoney
   end
 end
 
+#######################################
+#this class contains product data and is intended to
+#encapsulate product data and provide required operations
+#on the data. it also prints reports, though a view class
+#would potentially be a better mechanism for that once
+#requirements get more complicated.
 class ProductReport
-  #for now leaving public accessible
+  #for now leaving "public" via plain attr_accessor
+  #except for purchase so i can enforce its storage as array
+  #purchase can be accessed only via "add_purchase" below
   @title
   @description
   @brand
@@ -23,9 +34,13 @@ class ProductReport
   @full_price
   @purchases
 
-  attr_accessor :title,:description,:brand,:stock,:full_price,:purchases
+  attr_accessor :title,:description,:brand,:stock,:full_price
 
   include(ToyMoney)
+
+  def initialize
+    @purchases = []
+  end
 
   def do_prod_report
     puts ""
@@ -77,8 +92,15 @@ class ProductReport
     format(avg_discount)
   end
 
+  def add_purchase(purchase)
+    @purchases << purchase
+  end
+
 end
 
+#######################################
+#this class stores purchase data. a ProductReport
+#instance has zero or many Purchase object instances
 class Purchase
   @channel
   @date
@@ -90,6 +112,9 @@ class Purchase
   attr_accessor :channel,:date,:price,:shipping,:currency,:user
 end
 
+#######################################
+#this class stores user data. a Purchase object instance
+#has one user
 class User
   @name
   @state
@@ -97,6 +122,12 @@ class User
   attr_accessor :name,:state
 end
 
+#######################################
+#this object extracts brand report information
+#from the products, taking in its constructor
+#a list of products. i don't favor big constructor initializations
+#like this in c# or java or c++. is this considered an antipattern
+#in ruby? alternatively i can provide an init method
 class BrandReport
   include(ToyMoney)
 
@@ -104,12 +135,18 @@ class BrandReport
     @brands = {}
     prod_reports.each_value do |prod_report|
       brand = prod_report.brand.to_sym
+      toy_name = prod_report.title.delete(" ").to_sym
+      avg_price = prod_report.get_avg_sale_price.to_f
+      toy_revenue = prod_report.get_total_sales.to_f
+      toy_info = {
+          toy_name => {
+              :avg_toy_price => avg_price,
+              :toy_tot_revenue => toy_revenue
+          }
+      }
       if @brands.has_key?(brand)
         #don't add key twice but do add this products
         #information that we need
-        toy_name = prod_report.title.delete(" ").to_sym
-        avg_price = prod_report.get_avg_sale_price.to_f
-        toy_revenue = prod_report.get_total_sales.to_f
         toy_info = {
             :avg_toy_price => avg_price,
             :toy_tot_revenue => toy_revenue
@@ -123,16 +160,6 @@ class BrandReport
         end
 
       else
-        toy_name = prod_report.title.delete(" ").to_sym
-        avg_price = prod_report.get_avg_sale_price.to_f
-        toy_revenue = prod_report.get_total_sales.to_f
-
-        toy_info = {
-          toy_name => {
-              :avg_toy_price => avg_price,
-              :toy_tot_revenue => toy_revenue
-          }
-        }
         @brands[brand] = toy_info
       end
     end
@@ -173,7 +200,14 @@ class BrandReport
 
 end
 
-
+#######################################
+#so here i am loading this hash data in objects
+#that keep the data and provide methods for manipulating the
+#data according to report requirements. i'm using a hash for
+#prod_reports instead of an array because i want more practice
+#manipulating the hash api in ruby. i could have made this part
+#of an object as well (say, a reports manager type) but just
+# left this part procedural.
 def create_products(hash)
   prod_reports = {}
   hash["items"].each do |item|
@@ -184,7 +218,6 @@ def create_products(hash)
     report.stock = item["stock"]
     report.full_price = item["full-price"]
     if item["purchases"] && item["purchases"].length > 0
-      report.purchases = []
       item["purchases"].each do |purchase|
         purch = Purchase.new
         purch.channel = purchase["channel"]
@@ -195,7 +228,7 @@ def create_products(hash)
         purch.user = User.new
         purch.user.name = purchase['user']['name']
         purch.user.state = purchase['user']['state']
-        report.purchases << purch
+        report.add_purchase(purch)
       end
     end
     prod_reports[report.title] = report
@@ -220,7 +253,16 @@ puts "|_|                                       "
 # Calculate and print the average price the toy sold for
 # Calculate and print the average discount (% or $) based off the average sales price
 # Print the retail price of the toy
+
+#######################################
+#i'm calling a procedure to create my ProductReport objects
+#alternatively i could wrap the ProductReport and BrandReport types
+#in a facade pattern class and steer the operations from that.
 prod_reports = create_products(products_hash)
+
+#######################################
+#for each ProductReport object do the required report.
+#each instance creates the report of the data it contains
 prod_reports.each_value do |prodreport|
   prodreport.do_prod_report
 end
@@ -239,6 +281,14 @@ end
 # Count and print the number of the brand's toys we stock
 # Calculate and print the average price of the brand's toys
 # Calculate and print the total revenue of all the brand's toy sales combined
+
+#######################################
+#i only need one BrandReport
+#i instantiate it passing in the hash of ProductReport
+#objects, the data and methods of which are used to build
+#the brand report data and the report itself. because BrandReport
+#uses some methods of ProductReport, the methods BrandReport needs
+#are declared public in ProductReport
 brand_report = BrandReport.new(prod_reports)
 brand_report.do_brand_report
 
